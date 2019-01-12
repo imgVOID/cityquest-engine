1f73d5535e36226fa2c0a313e78ec4b21e1b9a09
Marie Nacht <imgvoid@i.ua>
2019-01-12 19:52:54 +0000

    heroku

--- map/settings.py
+++ map/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import os.path
-from urllib.parse import urlparse
+from urlparse import urlparse
 from django.conf.global_settings import LOGIN_REDIRECT_URL
 import django_heroku
 
