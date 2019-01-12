99de24e5e2ecb598907bf20cf43a851c28ad2e1b
Marie Nacht <imgvoid@i.ua>
2019-01-12 20:00:20 +0000

    heroku

--- map/settings.py
+++ map/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import os.path
-import urllib.parse as urlparse
+import urllib
 from django.conf.global_settings import LOGIN_REDIRECT_URL
 import django_heroku
 
@@ -113,7 +113,7 @@ DATABASES = {
 }
 
 # redis heroku
-redis_url = urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6959'))
+redis_url = urllib.parse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6959'))
 
 CACHES = {
     'default': {
