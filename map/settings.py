177c9e6292590a2ab38f800766716e390aad9167
Marie Nacht <imgvoid@i.ua>
2019-01-12 19:42:39 +0000

    heroku

--- map/settings.py
+++ map/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import os.path
+import urlparse
 from django.conf.global_settings import LOGIN_REDIRECT_URL
 import django_heroku
 
@@ -122,6 +123,7 @@ CACHES = {
             'DB': 0,
             'PASSWORD': redis_url.password,
         }
+    }
     }
 
 # Password validation
